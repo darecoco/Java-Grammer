6a77239f963a266056f131a63e63548e4d7b7b21
darecoco <66424222+darecoco@users.noreply.github.com>
2023-03-28 09:45:05 +0900

    주석추가

--- Pro01Inheritance/src/in09/GoodCalc.java
+++ Pro01Inheritance/src/in09/GoodCalc.java
@@ -2,22 +2,16 @@ package in09;
 
 public class GoodCalc extends Calculator{
 
-	public static void main(String[] args) {
-		GoodCalc c = new GoodCalc();
-		System.out.println(c.add(2, 3));
-		System.out.println(c.subtract(2,3));
-		System.out.println(c.average(new int[] {2, 3, 4}));
-	}
-
+//추상클래스(Calculator)에서 상속받은 추상메소드는 GoodCalc클래스에서 반드시 구현해야한다.
+//구현하지 않으면 컴파일 오류
+	
 	@Override
 	public int add(int a, int b) {
-		// TODO Auto-generated method stub
 		return a + b;
 	}
 
 	@Override
 	public int subtract(int a, int b) {
-		// TODO Auto-generated method stub
 		return a - b;
 	}
 
@@ -29,4 +23,11 @@ public class GoodCalc extends Calculator{
 		return sum / a.length;
 	}
 
+	public static void main(String[] args) {
+		GoodCalc c = new GoodCalc();
+		System.out.println(c.add(2, 3));
+		System.out.println(c.subtract(2,3));
+		System.out.println(c.average(new int[] {2, 3, 4}));
+	}
 }
+
