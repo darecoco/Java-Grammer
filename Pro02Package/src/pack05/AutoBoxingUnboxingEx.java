b1a4822a28d5a7a8906a90a67cac45ccbf4999d8
darecoco <66424222+darecoco@users.noreply.github.com>
2023-04-06 13:53:17 +0900

    자동 박싱, 자동 언박싱

---
+++ Pro02Package/src/pack05/AutoBoxingUnboxingEx.java
@@ -0,0 +1,14 @@
+package pack05;
+
+public class AutoBoxingUnboxingEx {
+
+	public static void main(String[] args) {
+		int n = 10;  //기본형 정수
+		Integer intObject = n;  //intObject객체 변수에 n값을 저장하면 자동 박싱이 됨.
+		System.out.println("intObject = " + intObject);
+		
+		int m = intObject + 10;  //객체변수 + 기본정수값 -> 기본형data로 저장
+		System.out.println("m = " + m);
+	}
+
+}
