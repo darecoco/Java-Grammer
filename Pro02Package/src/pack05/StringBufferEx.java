91db936d143b8847957b8abb3ed7eca38ba33d0c
darecoco <whaqlrh37@naver.com>
2023-04-11 09:24:43 +0900

    StringBuffer의 메소드들

--- Pro02Package/src/pack05/StringBufferEx.java
+++ Pro02Package/src/pack05/StringBufferEx.java
@@ -14,7 +14,7 @@ public class StringBufferEx {
 		sb.replace(8, 10, "your");	//sb의 문자열의 인덱스 8번째 부터 10번째 전까지의 문자열을 "your"로 교체
 		System.out.println(sb);		//sb = "This is your pencil"
 		
-		sb.delete(8, 13);			//sb가 가리키는 8번째 문자열에서 13-1의 문자열을 삭제
+		sb.delete(8, 13);			//sb가 가리키는 8번째 문자열에서 13번째 전까지의 문자열을 삭제
 		System.out.println(sb);		//sb = "This is pencil"
 		
 		sb.setLength(4);			//현sb의 길이는 14 인데, 4로 설정하면 앞 4글자만 남겨두고 뒤의 문자열이 삭제됨
