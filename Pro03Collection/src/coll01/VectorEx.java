71b9aafff2397fb22e505c543f3d97aad27e45e9
darecoco <whaqlrh37@naver.com>
2023-04-18 09:17:04 +0900

    Vector를 매개변수로 선언하는 법

--- Pro03Collection/src/coll01/VectorEx.java
+++ Pro03Collection/src/coll01/VectorEx.java
@@ -3,6 +3,14 @@ package coll01;
 import java.util.Vector;
 
 public class VectorEx {
+	public static void printVector(Vector<Integer> v2) { // Vector<Integer> v2 = v
+		//Vector<Integer> v2는 정수데이터를 담는 백터형 객체변수 선언
+		for(int i = 0; i < v2.size(); i++) {
+			int n = v2.get(i);
+			System.out.print(n + " ");
+		}//for
+	}
+	
 	public static void main(String args[]) {
 		Vector<Integer> v = new Vector<Integer>();
 		//기본 생성자에 의해서 10개의 객체를 저장할 공간 할당
@@ -16,15 +24,12 @@ public class VectorEx {
 		System.out.println("백터내의 요소 객체 수 : " + v.size());
 		System.out.println("백터내의 현재 용량 : " + v.capacity());
 		
+		printVector(v); //printVector()메소드를 호출
+		
 //		for(int i = 0; i < v.size(); i++) {
 //			int n = v.get(i);
-//			System.out.println(n);
+//			System.out.print(n + " ");
 //		}
-		
-		for(int i = 0; i < v.size(); i++) {
-			int n = v.get(i);
-			System.out.print(n + " ");
-		}
 		int sum = 0;
 		for(int i = 0; i < v.size(); i++) {
 			int n = v.elementAt(i);
