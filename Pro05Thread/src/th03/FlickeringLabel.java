89ddea68b6b6e019bc4480d9b39674f944b35fb6
darecoco <whaqlrh37@naver.com>
2023-06-15 14:08:30 +0900

    주석 설명 추가

--- Pro05Thread/src/th03/FlickeringLabel.java
+++ Pro05Thread/src/th03/FlickeringLabel.java
@@ -8,11 +8,15 @@ public class FlickeringLabel extends JLabel implements Runnable{
 	private long delay;
 	
 	public FlickeringLabel(String text, long delay) {
+		//1. flabel1 객체생성시 생성자 호출 할때 text="깜빡", delay=500
+		//2. flabel1 객체생성시 생성자 호출 할때 text="여기도 깜빡", delay=300
 		super(text);
+		// 지정된 텍스트("깜빡")로 레이블 인스턴슬르 만든다. 레이블 생성
 		this.delay = delay;
-		setOpaque(true);
-		Thread th = new Thread(this);
-		th.start();
+		setOpaque(true); //배경색을 설정(true)
+		Thread th = new Thread(this); // 1. this는 fLabel1을 나타냄
+										// 2. this는 fLabel2를 나타냄
+		th.start();//4. 스레드 실행
 	}
 	
 	@Override
@@ -20,13 +24,13 @@ public class FlickeringLabel extends JLabel implements Runnable{
 		int n = 0;
 		while(true) {
 			if(n==0)
-				setBackground(Color.CYAN);
+				this.setBackground(Color.CYAN);
 			else
 				setBackground(Color.orange);
 			if(n==0) n = 1;
 			else n = 0;
 			try {
-				Thread.sleep(delay);
+				Thread.sleep(delay); // delay = 5000 -> 0.5초 지연
 			}catch(InterruptedException e) {
 				return;
 			}
--- Pro05Thread/src/th03/FlickeringLabelEx.java
+++ Pro05Thread/src/th03/FlickeringLabelEx.java
@@ -10,11 +10,13 @@ public class FlickeringLabelEx extends JFrame{
 	public FlickeringLabelEx() {
 		setTitle("FlickeringLabelEx 예제");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c = getContentPane();
+		Container c = getContentPane(); //Container c = new Container(); ------------ 잘못된 예제
 		c.setLayout(new FlowLayout());
+		//컨테이너에 오브젝트를 어떻게 배치할 것인가.
 		
 		//깜빡이는 레이블 생성
-		FlickeringLabel fLabel = new FlickeringLabel("깜빡", 500);
+		FlickeringLabel fLabel1 = new FlickeringLabel("깜빡", 500);
+		//FlickringLabel클래스의 fLabel1 객체생성시 생성자 호출
 		
 		//깜빡이지 않는 레이블 생성
 		JLabel label = new JLabel("안깜빡");
@@ -22,11 +24,11 @@ public class FlickeringLabelEx extends JFrame{
 		//깜빡이는 레이블 생성2
 		FlickeringLabel fLabel2 = new FlickeringLabel("여기도 깜빡", 300);
 		
-		c.add(fLabel);
-		c.add(label);
-		c.add(fLabel2);
+		c.add(fLabel1); //컨테이너에 fLabel1이라는 객체를 배치
+		c.add(label); //컨테이너에 label이라는 객체를 배치
+		c.add(fLabel2); //컨테이너에 fLabel2이라는 객체를 배치
 		
-		setSize(300,150);
+		setSize(300,150); //컨테이너의 사이즈
 		setVisible(true);
 	}
 	
